1df4cacd5f2d489a99c60c5f0b5a77566ee0a6bf
Divyansh <divyanshracvik2209@gmail.com>
2024-07-20 11:45:31 +0530

    singup & login UI

--- bookwise/src/Landing.jsx
+++ bookwise/src/Landing.jsx
@@ -14,7 +14,7 @@ const Landing = () => {
           <h1 className='text-[#004AAD] text-center py-4 text-[1.5rem] md:text-[1.9rem]  font-semibold'>
             Books Simplified, Focus Amplified
           </h1>
-          <p className='text-[#2C8ED6] text-center text-[1.1rem] md:text-[1.3rem]'>
+          <p className='text-[#004AAD] text-center text-[1.1rem] md:text-[1.3rem]'>
             Your AI-powered reading companion, built to enhance focus and
             understanding for the social media generation.
           </p>
